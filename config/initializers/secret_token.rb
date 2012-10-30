3b7852928457d321f6067cbd4c4a119c5a081088
GesielRios <gesielrios@gmail.com>
2012-10-30 20:02:46 -0300

    Comite inicial do projeto de criacao da pagina do congresso da FAESF.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Congress::Application.config.secret_token = '0c64c753f9aed1e698dddc1eaf7de3c3ba3f19cc994f5760483d61377ef07c18a64b99a86d1f2ee99b048237d68d9cd26bb2246bb4882994a0dd55163bfacf05'
